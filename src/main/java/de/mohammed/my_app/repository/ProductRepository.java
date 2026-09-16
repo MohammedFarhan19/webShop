76846fe00db9e30f006d68d1c7ef9d4dcb1ea09a
MohammedFarhan19 <mohammed.farhan75@yahoo.de>
2026-09-16 16:48:43 +0200

    Extract method lowercaseTags

--- src/main/java/de/mohammed/my_app/repository/ProductRepository.java
+++ src/main/java/de/mohammed/my_app/repository/ProductRepository.java
@@ -7,18 +7,21 @@ import java.util.List;
 import de.mohammed.my_app.model.ProductResponse;
 
 public class ProductRepository {
+	
+	List<ProductResponse> products = Arrays.asList(
+			new ProductResponse("1", "AMD Ryzen 9 5950x", "nice processor", 79900,
+					Arrays.asList("AMD", "Processor")),
+			
+			new ProductResponse("2", "Intel Core i9", "another Processor 2", 37900,
+					Arrays.asList("Intel", "Processor")),
+			
+			new ProductResponse("3", "NVIDIA GeForce GTX", "Grafik karte", 74900,
+					Arrays.asList("NVIDIA2_changed", "Graphik")));
 
+	/*
+	 * Methode zum Ausgeben all Products
+	 */
 	public List<ProductResponse> findAll(String tag) {
-		List<ProductResponse> products = Arrays.asList(
-				new ProductResponse("1", "AMD Ryzen 9 5950x", "nice processor", 79900,
-						Arrays.asList("AMD", "Processor")),
-
-				new ProductResponse("2", "Intel Core i9", "another Processor 2", 37900,
-						Arrays.asList("Intel", "Processor")),
-
-				new ProductResponse("3", "NVIDIA GeForce GTX", "Grafik karte", 74900,
-						Arrays.asList("NVIDIA2_changed", "Graphik")));
-		
 		if (tag == null) {
 			return products;
 		} else {
@@ -26,16 +29,22 @@ public class ProductRepository {
 			List<ProductResponse> filtered = new ArrayList<>();
 			for (ProductResponse p : products) {
 
-				List<String> lowercaseTags = new ArrayList<>();
-
-				for (String t : p.getTags()) {
-					lowercaseTags.add(t.toLowerCase());
-				}
-				if (lowercaseTags.contains(tag)) {
+				if (lowercaseTags(p).contains(tag)) {
 					filtered.add(p);
 				}
 			}
 			return filtered;
 		}
+	}
+
+	/*
+	 * Methode um alles to lowercase 
+	 */
+	private List<String> lowercaseTags(ProductResponse p) {
+		List<String> lowercaseTags = new ArrayList<>();
+		for (String t : p.getTags()) {
+			lowercaseTags.add(t.toLowerCase());
+		}
+		return lowercaseTags;
 	}
 }
